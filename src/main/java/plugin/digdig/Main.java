0551065f1c7b6509a1379e2a91f356d86277315b
tai442 <uedataichi3@gmail.com>
2023-07-15 12:19:38 +0900

    インポートの最適化

--- src/main/java/plugin/digdig/Main.java
+++ src/main/java/plugin/digdig/Main.java
@@ -1,16 +1,6 @@
 package plugin.digdig;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.FireworkEffect;
-import org.bukkit.FireworkEffect.Type;
-import org.bukkit.World;
-import org.bukkit.entity.Firework;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
-import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.digdig.command.DigDigCommand;
 
